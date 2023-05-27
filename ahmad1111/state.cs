448cca59154d78aeb2bcf556b7e7506b680f5412
Ahmad-alhatamleh <129794610+Ahmad-alhatamleh@users.noreply.github.com>
2023-05-27 17:46:05 +0300

    Add files via upload

---
+++ ahmad1111/state.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ahmad1111
+{
+
+    public class state
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+    }
+}
